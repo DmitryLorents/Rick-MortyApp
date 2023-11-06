13a776e5f4e15bff239a70f71cabbfb03575be23
Dmitry Lorents <zmey.task@gmail.com>
2023-11-06 20:43:19 +0300

    Add logo & EpisodesView

---
+++ Rick&MortyApp/Modules/Episodes/View/EpisodesView.swift
@@ -0,0 +1,20 @@
+//
+//  EpisodesView.swift
+//  Rick&MortyApp
+//
+//  Created by Dmitry on 06.11.2023.
+//
+
+import UIKit
+
+class EpisodesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
