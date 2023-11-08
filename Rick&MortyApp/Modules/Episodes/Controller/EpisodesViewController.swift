97bade8b92485c2ee0b7448d811711c5109d0a4f
Dmitry Lorents <zmey.task@gmail.com>
2023-11-08 14:15:05 +0300

    Make delegates and dataSource for collection view

--- Rick&MortyApp/Modules/Episodes/Controller/EpisodesViewController.swift
+++ Rick&MortyApp/Modules/Episodes/Controller/EpisodesViewController.swift
@@ -9,12 +9,49 @@ import UIKit
 
 class EpisodesViewController: UIViewController {
     
+    //MARK: - Propperties
     let mainView = EpisodesView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view = EpisodesView()
+        setViews()
 
         
     }
+    
+    //MARK: - Methods
+    private func setViews() {
+        view = mainView
+        mainView.setDelegates(dataSource: self, delegate: self)
+        
+    }
+}
+//MARK: - CollectionView setup
+
+extension EpisodesViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    //    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    //        <#code#>
+    //    }
+    
+    //MARK: - FlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: view.bounds.width - (24*2), height: (view.bounds.width - (24*2) ) * (357.0 / 311.0) )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        55
+    }
+}
+
+extension EpisodesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: EpisodesCollectionViewCell.reuseId, for: indexPath) as? EpisodesCollectionViewCell else { return .init() }
+        return cell
+    }
+    
+    
 }
