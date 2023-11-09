f68c2553b7c89db6b9b4ea8ec21e2610e82b4bc8
Dmitry Lorents <zmey.task@gmail.com>
2023-11-09 22:32:00 +0300

    Adjust tableViewCell

--- Rick&MortyApp/Modules/CharacterDetails/View/Cell/DetailTableViewCell.swift
+++ Rick&MortyApp/Modules/CharacterDetails/View/Cell/DetailTableViewCell.swift
@@ -7,17 +7,52 @@
 
 import UIKit
 
-class DetailTableViewCell: UITableViewCell {
+final class DetailTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        contentView.backgroundColor = .green
+        setViews()
+        setConstraints()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+        super.setSelected(false, animated: animated)
 
         // Configure the view for the selected state
     }
-
+    
+    static let reuseId = String(describing: DetailTableViewCell.self)
+    
+    //MARK: - Parameters
+    
+    
+    
+    //MARK: - Init
+    
+    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        setViews()
+//        setConstraints()
+//    }
+    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    //MARK: - Methods
+    private func setViews() {
+        
+    }
+    
+    private func setConstraints() {
+        
+        
+       
+        NSLayoutConstraint.activate([
+            
+            
+            
+        ])
+    }
 }
