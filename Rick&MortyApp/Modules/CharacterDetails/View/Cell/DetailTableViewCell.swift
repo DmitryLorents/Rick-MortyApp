03306fefedbeed54990b263a81d0718d7ee5edbc
Dmitry Lorents <zmey.task@gmail.com>
2023-11-09 22:30:46 +0300

    Add swift files

---
+++ Rick&MortyApp/Modules/CharacterDetails/View/Cell/DetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailTableViewCell.swift
+//  Rick&MortyApp
+//
+//  Created by Dmitry on 09.11.2023.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
