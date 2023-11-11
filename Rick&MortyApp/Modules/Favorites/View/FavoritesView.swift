dbab4d4aa6dea1a18bbe626b3a4b6dbebbacc098
Dmitry Lorents <zmey.task@gmail.com>
2023-11-11 21:37:35 +0300

    Add images

---
+++ Rick&MortyApp/Modules/Favorites/View/FavoritesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesView.swift
+//  Rick&MortyApp
+//
+//  Created by Dmitry on 11.11.2023.
+//
+
+import UIKit
+
+class FavoritesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
