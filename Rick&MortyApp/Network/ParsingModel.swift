a0b2b04e4891863832293781eaf70528fb19b110
Dmitry Lorents <zmey.task@gmail.com>
2023-11-12 10:29:27 +0300

    Add parsing model

--- Rick&MortyApp/Network/ParsingModel.swift
+++ Rick&MortyApp/Network/ParsingModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+// MARK: - RickAndMorty
+struct RickAndMorty: Codable {
+//    let info: Info
+    let results: [Results]
+}
+
+//// MARK: - Info
+//struct Info: Codable {
+//    let count, pages: Int
+//    let next: String
+//    let prev: String?
+//}
+
+// MARK: - Result
+struct Results: Codable {
+    let id: Int
+    let name, airDate, episode: String
+    let characters: [String]
+    let url: String
+    let created: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case airDate = "air_date"
+        case episode, characters, url, created
+    }
+}
