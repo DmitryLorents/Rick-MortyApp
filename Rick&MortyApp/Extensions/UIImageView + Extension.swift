1778c6e9f5eea7bbcba2ca44c640a4cb1720548d
Dmitry Lorents <zmey.task@gmail.com>
2023-11-14 11:26:16 +0300

    Add extension to download images

---
+++ Rick&MortyApp/Extensions/UIImageView + Extension.swift
@@ -0,0 +1,36 @@
+//
+//  UIImageViewExtension.swift
+//  My-Recipes-Book
+//
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    func imageFromURL(_ URLString: String) {
+        
+        self.image = nil
+        //If imageurl's imagename has space then this line going to work for this
+        let imageServerUrl = URLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        
+        if let url = URL(string: imageServerUrl) {
+            URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                
+                if error != nil {
+                    DispatchQueue.main.async {
+                        self.image = UIImage(systemName: "photo")
+                    }
+                    return
+                }
+                DispatchQueue.main.async {
+                    if let data = data {
+                        if let downloadedImage = UIImage(data: data) {
+                            self.image = downloadedImage
+                        }
+                    }
+                }
+            }).resume()
+        }
+    }
+}
