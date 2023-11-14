e9b289fb6feac4b5c6f39a33a874d6277049127c
Dmitry Lorents <zmey.task@gmail.com>
2023-11-14 11:26:47 +0300

    Delete unnecessary code

--- Rick&MortyApp/Network/NetworkManager.swift
+++ Rick&MortyApp/Network/NetworkManager.swift
@@ -20,9 +20,9 @@ final class  NetworkManager {
     //MARK: - Parameters
     var urlString: String!
     let decoder = JSONDecoder()
-
+    
     //MARK: - Methods
-   private func getData <T: Codable> (urlString: String, parseProtocol: T.Type, completion: @escaping (Result<T, Error>) -> () ) {
+    private func getData <T: Codable> (urlString: String, parseProtocol: T.Type, completion: @escaping (Result<T, Error>) -> () ) {
         guard let url = URL(string: urlString) else {return}
         
         URLSession.shared.dataTask(with: url) { [weak self] (data, _, error) in
@@ -37,8 +37,8 @@ final class  NetworkManager {
                 completion(.failure(error!))
                 return}
             completion(.success(parsedData))
-            }.resume()
-        }
+        }.resume()
+    }
     
     func getEpisodes(completion: @escaping (Result<Episodes, Error>) -> () ) {
         let urlString = "https://rickandmortyapi.com/api/episode"
@@ -47,34 +47,6 @@ final class  NetworkManager {
     
     func getCharacter(with urlString: String, completion: @escaping (Result<CharacterData, Error>) -> () ) {
         getData(urlString: urlString, parseProtocol: CharacterData.self, completion: completion)
-    }
-    
-    func getImage(by URLString: String) -> UIImage {
-        var outputImage = UIImage()
-        
-        //If imageurl's imagename has space then this line going to work for this
-        let imageServerUrl = URLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
-        
-        if let url = URL(string: imageServerUrl) {
-            URLSession.shared.dataTask(with: url, completionHandler: { (data, _, error) in
-                
-                if error != nil {
-                    DispatchQueue.main.async {
-                        guard let image = UIImage(systemName: "photo") else {return}
-                                outputImage = image
-                    }
-                    return
-                }
-                DispatchQueue.main.async {
-                    if let data = data {
-                        if let downloadedImage = UIImage(data: data) {
-                            outputImage = downloadedImage
-                        }
-                    }
-                }
-            }).resume()
-        }
-        return outputImage
     }
     
 }
