4612e7b48bc6e23f83cd802abb81524aa80ea0a5
Dmitry Lorents <zmey.task@gmail.com>
2023-11-15 18:09:55 +0300

    Add setupCell method

--- Rick&MortyApp/Modules/CharacterDetails/View/Cell/DetailTableViewCell.swift
+++ Rick&MortyApp/Modules/CharacterDetails/View/Cell/DetailTableViewCell.swift
@@ -11,6 +11,7 @@ final class DetailTableViewCell: UITableViewCell {
     
     //MARK: - Parameters
     static let reuseId = String(describing: DetailTableViewCell.self)
+    
     private let mainLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.boldSystemFont(ofSize: 16)
@@ -30,12 +31,15 @@ final class DetailTableViewCell: UITableViewCell {
     
     
     
+   
+    //MARK: - Init
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setViews()
         setConstraints()
     }
-    //MARK: - Init
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -50,6 +54,11 @@ final class DetailTableViewCell: UITableViewCell {
     }
     
     //MARK: - Methods
+    
+    func setupCell(with mainTitle: String?, _ detailTitle: String?) {
+        mainLabel.text = mainTitle
+        detailLabel.text = detailTitle
+    }
     private func setViews() {
         contentView.addSubview(mainLabel)
         contentView.addSubview(detailLabel)
