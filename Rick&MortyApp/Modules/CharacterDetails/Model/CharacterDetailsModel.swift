319abb9bb575cfa1a46cbf4f50cf0c6675330c78
Dmitry Lorents <zmey.task@gmail.com>
2023-11-15 18:09:30 +0300

    Add model

--- Rick&MortyApp/Modules/CharacterDetails/Model/CharacterDetailsModel.swift
+++ Rick&MortyApp/Modules/CharacterDetails/Model/CharacterDetailsModel.swift
@@ -5,4 +5,8 @@
 //  Created by Dmitry on 15.11.2023.
 //
 
-import Foundation
+import UIKit
+
+enum CharacterDetailModel {
+    static let cellTitles = ["Gender", "Status", "Specie", "Origin", "Type", "Location"]
+}
