1613acb1951957e7643ae9c12bcacb0aa977b139
Dmitry Lorents <zmey.task@gmail.com>
2023-11-15 23:21:28 +0300

    Set launchView

--- Rick&MortyApp/Modules/LaunchScreen/View/LaunchView.swift
+++ Rick&MortyApp/Modules/LaunchScreen/View/LaunchView.swift
@@ -8,13 +8,42 @@
 import UIKit
 
 class LaunchView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    //MARK: - Parameters
+    private let logoImageView = UIImageView(image: UIImage(named: "logo"))
+    private let greenCircleImageView = UIImageView(image: UIImage(named: "greenCircle"))
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setViews()
+        setConstraints()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    //MARK: - Methods
+    
+    private func setViews() {
+        backgroundColor = .systemBackground
+        addSubview(logoImageView)
+        addSubview(greenCircleImageView)
+    }
+    
+    private func setConstraints() {
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        greenCircleImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: topAnchor, constant: 97),
+            logoImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            logoImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            
+            greenCircleImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            greenCircleImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            greenCircleImageView.widthAnchor.constraint(equalToConstant: 200),
+            greenCircleImageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
 }
