7fc2775b72d33daa44c31c44ad49504b723a15ee
Dmitry Lorents <zmey.task@gmail.com>
2023-11-15 23:20:37 +0300

    Add Launch module files

--- Rick&MortyApp/App/SceneDelegate.swift
+++ Rick&MortyApp/App/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let navVC = UINavigationController(rootViewController: TabBar())
+        let navVC = LaunchScreenViewController()//UINavigationController(rootViewController: TabBar())
         window?.rootViewController = navVC//TabBar()
         window?.makeKeyAndVisible()
     }
---
+++ Rick&MortyApp/Modules/LaunchScreen/View/LaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchView.swift
+//  Rick&MortyApp
+//
+//  Created by Dmitry on 15.11.2023.
+//
+
+import UIKit
+
+class LaunchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
