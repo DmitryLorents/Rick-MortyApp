9b779d18b2eb5cd57df294aca8fd199ad8e1d149
Dmitry Lorents <zmey.task@gmail.com>
2023-11-15 23:54:52 +0300

    Perform go to next screen

--- Rick&MortyApp/Modules/LaunchScreen/Controller/LaunchScreenViewController.swift
+++ Rick&MortyApp/Modules/LaunchScreen/Controller/LaunchScreenViewController.swift
@@ -9,21 +9,35 @@ import UIKit
 
 class LaunchScreenViewController: UIViewController {
 
+    //MARK: - Propperties
+    let mainView = LaunchView()
+    
+    
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        setViews()
+        goToNextVC()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        mainView.stopAnimation()
     }
-    */
+    
+    //MARK: - Methods
+    private func setViews() {
+        view = mainView
+        mainView.backgroundColor = .systemBackground
+    }
+    
+    private func goToNextVC() {
+        DispatchQueue.main.asyncAfter(deadline: .now()+3, execute: DispatchWorkItem(block: {
+            let tabBarVC = UINavigationController(rootViewController: TabBar())
+            tabBarVC.modalPresentationStyle = .fullScreen
+            self.present(tabBarVC, animated: true)
+        }))
+    }
+    
 
 }
